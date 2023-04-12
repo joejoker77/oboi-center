eecfb354499cd791d4ce2098168754629327c37c
Yanovskiy Vitaliy <joejoker77@gmail.com>
2023-04-12 10:55:57 +0300

    add migration for filter

---
+++ database/migrations/2023_04_12_074137_create_shop_filter_tables.php
@@ -0,0 +1,45 @@
+<?php
+
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_filters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('visible_in_categories');
+            $table->string('position')->default('left');
+        });
+
+        Schema::create('shop_filter_groups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('filter_group');
+            $table->foreign('filter_group')->references('id')->on('shop_filter_groups')->onDelete('CASCADE');
+            $table->string('name');
+            $table->json('categories')->nullable();
+            $table->json('tags')->nullable();
+            $table->json('attributes')->nullable();
+            $table->tinyInteger('display_header')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_filters');
+    }
+};
