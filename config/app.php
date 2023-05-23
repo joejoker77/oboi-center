50fcd564b138a8670128599c4c210768af245c5f
Yanovskiy Vitaliy <joejoker77@gmail.com>
2023-05-23 11:59:34 +0300

    fix styles

--- config/app.php
+++ config/app.php
@@ -193,6 +193,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\MetaTagsServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\SmsServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
